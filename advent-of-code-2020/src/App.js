d8d118ab420264c78fba9f7dee5789a665700e35
happycod3r <happycod3r@users.noreply.github.com>
2020-12-03 17:52:55 +0000

    adding day 1

--- advent-of-code-2020/src/App.js
+++ advent-of-code-2020/src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+
+
+function Calculate(){
+    const input = document.getElementsByTagName('input')[0];
+
+    if(input){
+     const inputList = input.value.split(' ');
+    
+      const testingList = inputList;
+
+     inputList.forEach((item, index) => {
+         testingList.forEach((testItem, index) => {
+                const sum = Number(item) + Number(testItem);
+                if(sum === 2020){
+                    const result = Number(item) * Number(testItem);
+                    document.getElementsByClassName('output')[0].innerHTML = result;
+                }
+            })
+        });
+    }
+}
+
+function Calculate2(){
+    const input = document.getElementsByTagName('input')[0];
+
+    if(input){
+     const inputList = input.value.split(' ');
+    
+      const testingList = inputList;
+
+     inputList.forEach((item, index) => {
+         testingList.forEach((testItem, index) => {
+                const sum = Number(item) + Number(testItem);
+                if(sum === 2020){
+                    const result = Number(item) * Number(testItem);
+                    document.getElementsByClassName('output')[0].innerHTML = result;
+                }
+            })
+        });
+    }
+}
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+            <div>
+            <h1>Day 1</h1>
+                <input type='text' id='inputId' className='input'/>
+                <button className='button' onClick={() => Calculate()}>
+                    Go
+                </button>
+                <br/>
+                <div className='output'/>
+            </div>
+            <div>
+            <h1>Day 2</h1>
+                <input type='text' id='inputId' className='input'/>
+                <button className='button' onClick={() => Calculate2()}>
+                    Go
+                </button>
+                <br/>
+                <div className='output'/>
+            </div>
+
+        </div>
   );
 }
 
