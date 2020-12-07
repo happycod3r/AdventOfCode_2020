ecb6b73c8cfbc37444102fc437f8a3aa113eae87
happycod3r <happycod3r@users.noreply.github.com>
2020-12-07 10:22:17 +0000

    adding day 7

--- Dart/advent_of_code_2020/bin/app.dart
+++ Dart/advent_of_code_2020/bin/app.dart
@@ -1,6 +1,7 @@
-import 'package:advent_of_code_2020/day6.dart';
+import '../lib/day7.dart';
 
 void main(List<String> arguments) {
   //print('Day 6.1 result is: ' + Day6.getYesSum().toString());
-  print('Day 6.2 result is: ' + Day6.getYesSum2().toString());
+  //print('Day 6.2 result is: ' + Day6.getYesSum2().toString());
+  print('Day 7.1 result is: ' + Day7.getBagCount().toString());
 }
--- Dart/advent_of_code_2020/lib/day7.dart
+++ Dart/advent_of_code_2020/lib/day7.dart
@@ -1,3 +1,92 @@
 import 'dart:io';
 
-class Day7 {}
+class Day7 {
+  static List<Map<dynamic, dynamic>> parseList(List<String> list) {
+    List<Map<dynamic, dynamic>> resultList = [];
+
+    list.forEach((element) {
+      var parsedElem = element.split(' bags contain ');
+      var listOfContent = parsedElem[1].split(', ');
+
+      listOfContent[listOfContent.length - 1] =
+          listOfContent[listOfContent.length - 1].contains('.')
+              ? listOfContent[listOfContent.length - 1].substring(
+                  0, listOfContent[listOfContent.length - 1].length - 1)
+              : listOfContent[listOfContent.length - 1];
+
+      resultList.add({
+        'bag': parsedElem[0],
+        'content': listOfContent,
+      });
+    });
+
+    return resultList;
+  }
+
+  static List<Map<dynamic, dynamic>> getMatches(
+      List<Map<dynamic, dynamic>> input, String searchWord) {
+    List<Map<dynamic, dynamic>> result = [];
+
+    input.forEach((rule) {
+      (rule['content'] as List).forEach((content) {
+        if (content.toString().contains(searchWord)) {
+          result.add(rule);
+        }
+      });
+    });
+
+    return result;
+  }
+
+  static int getBagCount() {
+    var input = File('./lib/input/day7').readAsStringSync();
+
+    var searchWord = 'shiny gold';
+    // var counter = 0;
+
+    var listOfRules = input.split('\r\n');
+
+    var matchedRules = [];
+    var level1 = [];
+    var level2 = [];
+    var level3 = [];
+
+    var listParsed = parseList(listOfRules);
+
+    // * contain at least one
+    matchedRules = getMatches(listParsed, searchWord);
+
+    level1.addAll(matchedRules);
+    level2.addAll(matchedRules);
+    level3.addAll(matchedRules);
+
+    matchedRules.forEach((element) {
+      level1.addAll(getMatches(listParsed, element['bag']));
+    });
+
+    level2.addAll(level1);
+    level3.addAll(level1);
+
+    level1.forEach((element) {
+      level2.addAll(getMatches(listParsed, element['bag']));
+    });
+
+    level3.addAll(level2);
+
+    level2.forEach((element) {
+      level3.addAll(getMatches(listParsed, element['bag']));
+    });
+
+    // parsedList.forEach((rule) {
+    //   (rule['content'] as List).forEach((content) {
+    //     directRules.forEach((directRule) {
+    //       if (content.toString().contains(directRule['bag'])) {
+    //         counter++;
+    //       }
+    //     });
+    //   });
+    // });
+
+    return 0;
+  }
+}
