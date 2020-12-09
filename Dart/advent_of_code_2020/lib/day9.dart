a30ad3a7a1621671c9c3f964074b91a962593360
José dos Santos <happycod3r@gmail.com>
2020-12-09 22:47:57 +0000

    day 9.1

--- Dart/advent_of_code_2020/lib/day9.dart
+++ Dart/advent_of_code_2020/lib/day9.dart
@@ -1,15 +1,46 @@
 import 'dart:io';
 
 class Day9 {
-  static dynamic parseInput(String input) {
+  static List parseInput(String input) {
     var lines = input.split('\r\n');
+    return lines.map((e) => int.parse(e)).toList();
+  }
+
+  static Map<dynamic, dynamic> getSumCombination(List preamble, number) {
+    var combination;
+    preamble.forEach((number1) {
+      preamble.forEach((number2) {
+        if ((number1 + number2) == number && number1 != number2) {
+          combination = {number1: number2};
+        }
+      });
+    });
+    return combination;
   }
 
   static int first() {
-    var input = File('./lib/input/day8').readAsStringSync();
+    var input = File('./lib/input/day9').readAsStringSync();
     var parsedInput = parseInput(input);
 
     var result = 0;
+
+    for (var i = 25; i < parsedInput.length; i++) {
+      /*var preamble = List.generate(parsedInput.length, (index) {
+        if (index >= i - 25 && index < i) {
+          return parsedInput[index];
+        }
+      });*/
+
+      var preamble = List(25);
+      List.copyRange(preamble, 0, parsedInput, i - 25, i);
+
+      var combination = getSumCombination(preamble, parsedInput[i]);
+
+      if (combination == null) {
+        result = parsedInput[i];
+        break;
+      }
+    }
 
     return result;
   }
