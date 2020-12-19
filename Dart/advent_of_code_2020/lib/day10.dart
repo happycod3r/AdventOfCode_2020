874b7b1112cb5775e252629db7089516488309d7
José dos Santos <happycod3r@gmail.com>
2020-12-19 13:19:42 +0000

    day 10

--- Dart/advent_of_code_2020/lib/day10.dart
+++ Dart/advent_of_code_2020/lib/day10.dart
@@ -6,10 +6,7 @@ class Day10 {
     var parsedInput = parseInput(input);
     parsedInput.sort();
 
-    var seatJoltage = 0;
-    var deviceJoltage = parsedInput.last + 3;
-
-    var diffs = getJoltDiffs(seatJoltage, deviceJoltage, parsedInput);
+    var diffs = getJoltDiffs(parsedInput);
 
     return diffs.keys.first * diffs.values.first;
   }
@@ -19,8 +16,7 @@ class Day10 {
     return lines.map((e) => int.parse(e)).toList();
   }
 
-  static Map<int, int> getJoltDiffs(
-      int seatJoltage, int deviceJoltage, List<int> input) {
+  static Map<int, int> getJoltDiffs(List<int> input) {
     var oneJoltDiffs = 0;
     var threeJoltDiffs = 0;
 
@@ -40,14 +36,67 @@ class Day10 {
     return {oneJoltDiffs: threeJoltDiffs};
   }
 
+  static List<int> createShorterArrangement(List<int> input) {
+    var newList = [...input];
+    var i = 0;
+    while (i < newList.length - 1) {
+      if (i < newList.length - 3 && newList[i + 3] - newList[i] == 3) {
+        newList.removeAt(i + 1);
+      }
+      i++;
+    }
+    return newList;
+  }
+
+  static List<int> getDiffs(List<List<int>> arrangements) {
+    var diffs = <int>[];
+    var i = 0;
+    while (i < arrangements[1].length - 1) {
+      if (arrangements[1][i] != arrangements[0][i]) {
+        diffs.add(arrangements[0][i]);
+      }
+      i++;
+    }
+    return diffs;
+  }
+
+  static List<List<int>> createArrangements(
+      List<int> input, List<List<int>> arrangements, List<int> diffs) {
+    var newList = [...input];
+    for (var e in diffs) {
+      newList.add(e);
+      var arrg = arrangements.firstWhere((element) => element == newList,
+          orElse: () => null);
+      if (arrg != null) {
+        break;
+      } else {
+        arrangements.add(newList);
+      }
+    }
+    arrangements.addAll(createArrangements(newList, arrangements, diffs));
+    return arrangements;
+  }
+
+  static List<List<int>> getArrangements(List<int> input) {
+    var listOfArrangements = <List<int>>[];
+
+    listOfArrangements.add(input);
+    listOfArrangements.add(createShorterArrangement(input));
+    var diffs = getDiffs(listOfArrangements);
+
+    var list =
+        createArrangements(listOfArrangements[1], listOfArrangements, diffs);
+
+    return listOfArrangements;
+  }
+
   static int second() {
     var input = File('./lib/input/day10').readAsStringSync();
     var parsedInput = parseInput(input);
     parsedInput.sort();
 
-    var seatJoltage = 0;
-    var deviceJoltage = parsedInput.last + 3;
+    var arrangements = getArrangements(parsedInput);
 
-    return -1;
+    return arrangements.length;
   }
 }
